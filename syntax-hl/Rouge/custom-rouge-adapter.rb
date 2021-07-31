d8dd127859c92e4ab38e470efc228394e2724ce8
tajmone <tajmone@libero.it>
2021-07-31 02:43:41 +0200

    Rouge Adapter v1.0.0
    First official version of our custom Rouge adapter for ALAN.
    Code tweaked to ensure that the adapter extension will always try to
    load our custom lexer from the same folder where the adapter is located.
    Added credits to @mojavelinux in header comments.

--- syntax-hl/Rouge/custom-rouge-adapter.rb
+++ syntax-hl/Rouge/custom-rouge-adapter.rb
@@ -1,10 +1,20 @@
+# Rouge Adapter for ALAN                v1.0.0 | 2021/07/30 | by Tristano Ajmone
+# ==============================================================================
+# Created with the kind support of Dan Allen (@mojavelinux) from the Asciidoctor
+# Project, who helped me out with with this when I was stuck. Thanks Dan!
+#
+#   https://github.com/asciidoctor/asciidoctor/issues/4080
+# ==============================================================================
+
 class CustomRougeAdapter < (Asciidoctor::SyntaxHighlighter.for 'rouge')
   register_for 'rouge'
 
   # Defer loading Rouge until the `load_library` method is called:
   def load_library
     require 'rouge'
-    require './alan3.rb'
+    # The "alan3.rb" lexer must be in same folder as this script!
+    herepath=File.expand_path(File.dirname(__FILE__))
+    require "#{herepath}/alan3.rb"
     require './themes/alan-tester.rb' # Custom theme for tests.
     :loaded
   end
